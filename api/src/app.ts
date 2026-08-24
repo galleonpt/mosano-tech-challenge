c4d931e7d2a4425b886fe971b7e82165444bcb05
galleonpt <jose15.luis00@gmail.com>
2026-08-24 01:26:44 +0100

    api: add cors

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,3 +1,4 @@
+import cors from 'cors';
 import 'dotenv/config';
 import express from 'express';
 import mongoose from 'mongoose';
@@ -7,6 +8,7 @@ const app = express();
 const PORT = process.env.PORT ?? 3333;
 
 app.use(express.json());
+app.use(cors());
 app.use(router);
 
 const mongoUri = `mongodb://${process.env.MONGO_ROOT_USERNAME}:${process.env.MONGO_ROOT_PASSWORD}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DB_NAME}?authSource=admin`;
